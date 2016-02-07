3806fd311c14d31af880fa0b6ef8e9af9cbdc521
Razu Kc <kc.razu@gmail.com>
2016-02-07 12:18:38 +0545

    Mailer config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: 'clockb-demo.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'clockb-demo.herokuapp.com', port: '25' }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
