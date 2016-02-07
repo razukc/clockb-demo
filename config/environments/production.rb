ee1d97a30821e4fc0f9c83c9070f554eff465af8
Razu Kc <kc.razu@gmail.com>
2016-02-07 13:19:11 +0545

    Main config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,11 +77,11 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  # Custom settings
-    config.action_mailer.default_url_options = { host: 'clockb-demo.herokuapp.com' }
-    config.action_mailer.delivery_method = :smtp
+# Custom settings
+config.action_mailer.default_url_options = { host: 'clockb-demo.herokuapp.com' }
+config.action_mailer.delivery_method = :smtp
 config.action_mailer.perform_deliveries = true
-config.action_mailer.raise_delivery_errors = false
+config.action_mailer.raise_delivery_errors = true
 config.action_mailer.default :charset => "utf-8"
 
 config.action_mailer.smtp_settings = {
