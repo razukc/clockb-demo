fcb86b2666d7550c550be50e69ac7850cd381839
Razu Kc <kc.razu@gmail.com>
2016-02-07 13:24:29 +0545

    Main config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,10 +87,13 @@ config.action_mailer.default :charset => "utf-8"
 config.action_mailer.smtp_settings = {
 address: "smtp.gmail.com",
 port: 587,
-domain: ENV["GMAIL_DOMAIN"],
 authentication: "plain",
 enable_starttls_auto: true,
+# user_name: ENV["GMAIL_USERNAME"],
+# domain: ENV["GMAIL_DOMAIN"],
+# password: ENV["GMAIL_PASSWORD"]
 user_name: ENV["GMAIL_USERNAME"],
-password: ENV["GMAIL_PASSWORD"]
+domain: ENV["GMAIL_DOMAIN"],
+password: "ba24pa509"
 }
 end
