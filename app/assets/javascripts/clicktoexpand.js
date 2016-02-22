0bac02c0f48e1ed3be7ae782adf72664951f92ac
Razu Kc <kc.razu@gmail.com>
2016-02-22 12:20:09 +0545

    Read more

--- app/assets/javascripts/clicktoexpand.js
+++ app/assets/javascripts/clicktoexpand.js
@@ -1,8 +1,8 @@
     $(function(){
       setTimeout(function(){
         $('.readmore').readmore({
-          moreLink: '<a href="#" class="button tiny secondary right" style="width:auto;">Read More</a>',
-          lessLink: '<a href="#" class="button tiny secondary right" style="width:auto;">Close</a>',
+          moreLink: '<a href="#" class="button tiny info right clearfix" style="width:auto;margin-top:10px;">Read More</a>',
+          lessLink: '<a href="#" class="button tiny info right clearfix" style="width:auto;">Close</a>',
           afterToggle: function(trigger, element, expanded) {
               // if(! expanded) { // The "Close" link was clicked
               //   $('html, body').animate( { scrollTop: element.offset().top }, {duration: 100 } );
