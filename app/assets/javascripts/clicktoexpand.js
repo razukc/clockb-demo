851debe35d258e51a4c009671fa6cb4e04680b6d
Razu Kc <kc.razu@gmail.com>
2016-03-03 22:51:48 +0545

    read more button

--- app/assets/javascripts/clicktoexpand.js
+++ app/assets/javascripts/clicktoexpand.js
@@ -1,11 +1,11 @@
     $(function(){
       setTimeout(function(){
         $('.readmore').readmore({
-          moreLink: '<a href="#" class="button tiny info right clearfix" style="width:auto;margin-top:10px;">Read More</a>',
+          moreLink: '<a href="#" class="button tiny info right clearfix" style="width:auto;">Read More</a>',
           lessLink: '<a href="#" class="button tiny info right clearfix" style="width:auto;">Close</a>',
           afterToggle: function(trigger, element, expanded) {
               if(expanded){
-                
+
                 //$('.request-button').css({'margin-top': '0px'});
               }
               $(document).foundation('equalizer', 'reflow');
