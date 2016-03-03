c897512ca0a46f3e70ab875ab7a20dec9e2ec19c
Razu Kc <kc.razu@gmail.com>
2016-03-03 23:15:18 +0545

    Event slider

--- app/admin/events_slider.rb
+++ app/admin/events_slider.rb
@@ -3,12 +3,20 @@ ActiveAdmin.register EventsSlider do
 	config.batch_actions = false
 config.filters = false
 index download_links: false
-permit_params :image, :caption, :add_to_site
+permit_params :image, :image_cache, :remove_image, :caption, :add_to_site
 
 form :html => { :multipart => true } do |f|
 	f.semantic_errors *f.object.errors.keys
 	f.inputs "Slider Details" do
-	f.input :image, as: :file
+	if f.object.image && f.object.image.file
+	  li label "Event Slider", class: 'label' do
+	  dd image_tag f.object.image_url(:thumb)
+	  f.input :remove_image, as: :boolean, :label => "Remove"
+	end
+	else
+	  f.input :image, as: :file, :label => "Event Slider"
+	  f.input :image_cache, as: :hidden
+	end	
 	f.input :caption
 	f.input :add_to_site
 	end
