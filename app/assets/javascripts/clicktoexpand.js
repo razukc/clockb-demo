9e9d8223bed01679f6addaf4c6020775b1ef2563
Razu Kc <kc.razu@gmail.com>
2016-03-03 18:53:43 +0545

    changes

--- app/assets/javascripts/clicktoexpand.js
+++ app/assets/javascripts/clicktoexpand.js
@@ -4,10 +4,11 @@
           moreLink: '<a href="#" class="button tiny info right clearfix" style="width:auto;margin-top:10px;">Read More</a>',
           lessLink: '<a href="#" class="button tiny info right clearfix" style="width:auto;">Close</a>',
           afterToggle: function(trigger, element, expanded) {
-              // if(! expanded) { // The "Close" link was clicked
-              //   $('html, body').animate( { scrollTop: element.offset().top }, {duration: 100 } );
-              // }
-          $(document).foundation('equalizer', 'reflow');
+              if(expanded){
+                
+                //$('.request-button').css({'margin-top': '0px'});
+              }
+              $(document).foundation('equalizer', 'reflow');
           }
         });  
           $(document).foundation('equalizer', 'reflow');
