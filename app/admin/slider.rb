856385ee75be500f4902840055159a1e045f4091
Razu Kc <kc.razu@gmail.com>
2016-03-03 19:19:59 +0545

    cloudinary

--- app/admin/slider.rb
+++ app/admin/slider.rb
@@ -3,7 +3,7 @@ menu parent: "Sliders"
 config.batch_actions = false
 
 index :download_links => false, as: :grid do |slider|
-  dd link_to image_tag(slider.slide.thumb), edit_admin_slider_path(slider)
+  dd link_to image_tag(slider.slide_url(:thumb)), edit_admin_slider_path(slider)
   # dd link_to "Edit", edit_admin_slider_path(slider) 
   # dd link_to "Delete", admin_slider_path(slider), method: :delete, data: { :confirm => "Are you sure?" }
 end	
