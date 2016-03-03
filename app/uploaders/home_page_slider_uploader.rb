c6a72366686ae6e29d7dc09318fd43aa7bc0fb4a
Razu Kc <kc.razu@gmail.com>
2016-03-03 19:25:14 +0545

    home page sliders

--- app/uploaders/home_page_slider_uploader.rb
+++ app/uploaders/home_page_slider_uploader.rb
@@ -1,7 +1,7 @@
 # encoding: utf-8
 
 class HomePageSliderUploader < CarrierWave::Uploader::Base
-  # include Cloudinary::CarrierWave
+  include Cloudinary::CarrierWave
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
