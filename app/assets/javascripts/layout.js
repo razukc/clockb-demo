7efab3b6440af120d9dd4f092ca6e762e29ffcc8
Razu Kc <kc.razu@gmail.com>
2016-03-14 22:56:53 +0545

    application.js renamed variable

--- app/assets/javascripts/layout.js
+++ app/assets/javascripts/layout.js
@@ -1,9 +1,9 @@
 $(function(){
 	$(window).bind('load resize', function () { 
-	    screen = $('div.full').width();
+	    screen1 = $('div.full').width();
 	    content = 1000;
-		remaining = screen - content;
-		fluid =  (remaining/screen)*50;
+		remaining = screen1 - content;
+		fluid =  (remaining/screen1)*50;
 		$('div.column-fluid').css({'width': fluid+'%'});
 		// $('div.column-fluid').animate({width: fluid+'%'},600, "linear");
 		$('div.full').animate({opacity: 1}, 800)
