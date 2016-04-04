f137596e07eff822bebb5ef9971c81a9f9b0baee
Razu Kc <kc.razu@gmail.com>
2016-04-04 20:01:08 +0545

    logo and images

--- app/uploaders/slider_uploader.rb
+++ app/uploaders/slider_uploader.rb
@@ -54,7 +54,17 @@ include Cloudinary::CarrierWave
   def extension_white_list
     %w(jpg jpeg gif png)
   end
-
+  def content_type_whitelist
+    /image\//
+  end
+  def filename
+     "#{secure_token}.#{file.extension}" if original_filename.present?
+  end
+  protected
+  def secure_token
+    var = :"@#{mounted_as}_secured_token"
+    model.instance_variable_get(var) or model.instance_variable_set(var, SecureRandom.uuid)
+  end
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
   # def filename
