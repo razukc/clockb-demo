99a4420512654f0d7c089eba3dde40ad0c93d1e9
Razu Kc <kc.razu@gmail.com>
2016-04-04 22:12:24 +0545

    scope newtworkers

--- app/models/usermeetup.rb
+++ app/models/usermeetup.rb
@@ -2,6 +2,6 @@ class Usermeetup < ActiveRecord::Base
   belongs_to :user
 
 def self.has_requested_meetup(user_id, target_user)
-Usermeetup.where(:user_id => user_id, :user_x => target_user).limit(1)
+Usermeetup.where(:user_id => user_id, :user_x => target_user, :meetup => true).limit(1)
 end
 end
